c82a3fe204f20b985fe058dcc7ad7ce0c50d7486
mariabarrios <gabriela.lucyana15@gmail.com>
2024-02-29 11:16:55 -0500

    version depurada

--- app/mostrarContenido.js
+++
@@ -1,9 +0,0 @@
-const contenido = $(".contenido");
-export function mostrarContenido(){
-    contenido.html('<h4 class = "text-white">MOSTRANDO CONTENIDO !!</h4>');
-
-};
-
-export function mostrarContenidoVacio(){
-    contenido.html('<h4 class = "text-white">TIENES QUE INICIAR SESION PARA VER !!</h4>');
-};
